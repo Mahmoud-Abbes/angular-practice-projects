da099a8121eda40839d6847ced70241cac928659
Mahmoud-Abbes <mahmoud.abbes.155@gmail.com>
2026-02-11 08:24:17 +0100

    feat(01-profile-card): implement profile card in app component

--- 01-profile-card/src/app/app.ts
+++ 01-profile-card/src/app/app.ts
@@ -7,5 +7,8 @@ import { Component, signal } from '@angular/core';
   styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('profile-card');
+  name: string = 'Lorem';
+  age: number | null = 22;
+  description: string =
+    'Duis ex reprehenderit ea deserunt Ut laboris minim pariatur Lorem et excepteur ullamco eu aliquip eiusmod proident eu dolore.';
 }
