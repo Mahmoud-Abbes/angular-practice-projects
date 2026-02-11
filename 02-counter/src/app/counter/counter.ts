358a6ce1dd49ca2de5ecceb685ce3488d6c4af02
Mahmoud-Abbes <mahmoud.abbes.155@gmail.com>
2026-02-11 09:56:53 +0100

    feat(02-counter): implement counter component with increment/decrement functionality

---
+++ 02-counter/src/app/counter/counter.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  imports: [],
+  templateUrl: './counter.html',
+  styleUrl: './counter.scss',
+})
+export class Counter {
+  counter: number = 0;
+
+  increment(){
+    this.counter++;
+  }
+
+  decrement(){
+    this.counter--;
+  }
+}
